4c1fb8e5a7d3af7834bc7f00ea58a717267e2079
GabrielGosselin <snow.gabo51@gmail.com>
2024-04-10 12:47:47 -0400

    setup landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { motion } from "framer-motion";
+import hero from "./img/hero5.png";
+import logo from './img/blueLogo.PNG'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <motion.img initial={{ y: -20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.3 }} className="logo" src={logo} alt="logo" />
       </header>
+      <section className="App-main">
+        <motion.h1 className="title"
+          initial={{ y: -20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.5 }}
+        >
+          Commercial real estate financing, simplified
+        </motion.h1>
+      </section>
+      <footer className="frame">
+        <motion.img initial={{scaleY:0.9, y: 20, opacity: 0 }}
+          animate={{scaleY:1, y: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.7 }} className="desktop" src={hero} alt="frame of mobile"/>
+      </footer>
     </div>
   );
 }
